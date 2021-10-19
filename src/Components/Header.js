9531d8e5f9af13d5cc7fe6566f84fbbaff35cd97
eackerma <39926240+eackerma@users.noreply.github.com>
2021-10-19 00:57:01 -0700

    Changes backs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,38 +14,7 @@ class Header extends Component {
     return (
       <header id="home">
         <ParticlesBg color ="#0C3C46" num={200} type="color" bg={true} />
-          let config = {
-              num: [4, 7],
-              rps: 0.1,
-              radius: [5, 40],
-              life: [1.5, 3],
-              v: [2, 3],
-              tha: [-40, 40],
-              // body: "./img/icon.png", // Whether to render pictures
-              // rotate: [0, 20],
-              alpha: [0.6, 0],
-              scale: [1, 0.1],
-              position: "center", // all or center or {x:1,y:1,width:100,height:100}
-              color: ["random", "#ff0000"],
-              cross: "dead", // cross or bround
-              random: 15,  // or null,
-              g: 5,    // gravity
-              // f: [2, -1], // force
-              onParticleUpdate: (ctx, particle) => {
-                  ctx.beginPath();
-                  ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
-                  ctx.fillStyle = particle.color;
-                  ctx.fill();
-                  ctx.closePath();
-              }
-            };
-
-    return (
-      <div>
-        <SignIn />
-        <ParticlesBg type="custom" config={config} bg={true} />
-      </div>
-    ) 
+          
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
